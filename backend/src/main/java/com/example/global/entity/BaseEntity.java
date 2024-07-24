59d9508d7d4044f7ab8ac6a5d3b86e85f464ee2f
박민준 <ppmm98@naver.com>
2024-07-24 14:58:36 +0900

    chore: camelCase, remove id

--- backend/src/main/java/com/example/global/entity/BaseEntity.java
+++ backend/src/main/java/com/example/global/entity/BaseEntity.java
@@ -1,8 +1,6 @@
 package com.example.global.entity;
 
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,9 +12,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-
-
 @MappedSuperclass // 객체 입장에서 공통 매핑 정보가 필요할 때 사용
 @SuperBuilder(toBuilder = true) //
 @EntityListeners(AuditingEntityListener.class)
@@ -24,11 +19,8 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Getter
 @ToString
 public abstract class BaseEntity {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
     @CreatedDate
-    private LocalDateTime created_at;
+    private LocalDateTime createdAt;
     @LastModifiedDate
-    private LocalDateTime updated_at;
+    private LocalDateTime updatedAt;
 }
