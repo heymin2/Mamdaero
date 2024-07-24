5968f6f46a3c9ca7eec8f32a899cc1c51c65f81a
허세령 <seryoii@naver.com>
2024-07-24 10:33:46 +0900

    add: frontend 라이브러리 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,16 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
+import './styles/index.css';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <div className="text-3xl font-bold underline">test</div>
+        <div className="card">
+          <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
