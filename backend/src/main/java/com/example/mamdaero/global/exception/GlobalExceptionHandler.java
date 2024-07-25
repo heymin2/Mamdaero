89b9acc67c38fa472c12d6ad0039254557655617
신혜민 <alddlv81@gmail.com>
2024-07-25 16:21:19 +0900

    fix: global 분리된 폴더 수정

--- backend/src/main/java/com/example/mamdaero/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/mamdaero/global/exception/GlobalExceptionHandler.java
@@ -1,15 +1,25 @@
-package com.example.global.exception;
+package com.example.mamdaero.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(CustomException.class)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exception(Exception ex){
+        return new ResponseEntity<>("알 수 없는 에러 발생", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({CustomException.class})
     public ResponseEntity<?> customExceptionHandler(CustomException e) {
         ExceptionConstants ec = e.getConstants();
+        log.error("Error handler : {}", ec.getCode());
         return new ResponseEntity<>(ec.getCode(), ec.getStatus());
     }
+
 }
