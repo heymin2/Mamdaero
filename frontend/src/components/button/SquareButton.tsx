0c2f988eae083fb6af0991d645774df8028d4f85
허세령 <seryoii@naver.com>
2024-07-25 09:49:48 +0900

    add: 둥근 버튼, 네모 버튼, 폰트 추가

---
+++ frontend/src/components/button/SquareButton.tsx
@@ -0,0 +1,27 @@
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+  size: 'sm' | 'md' | 'lg';
+  user: 'counselor' | 'client' | 'common';
+}
+
+const Button = ({ label, onClick, size, user }: ButtonProps) => {
+  const sizeClasses = {
+    sm: 'w-20 h-7 text-base font-bold',
+    md: 'w-24 h-9 text-lg font-bold',
+    lg: 'w-40 h-10 text-xl font-bold',
+  };
+
+  const userClasses = {
+    counselor: 'bg-blue-200',
+    client: 'bg-orange-200',
+    common: 'bg-gray-200',
+  };
+  return (
+    <button onClick={onClick} className={`rounded-md ${sizeClasses[size]} ${userClasses[user]}`}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;
