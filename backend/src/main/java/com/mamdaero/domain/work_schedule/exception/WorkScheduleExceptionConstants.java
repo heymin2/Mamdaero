728c6b4b13f2dc9f441255d9aba1faa274a65f92
박형준 <issg2030@gmail.com>
2024-07-28 18:30:03 +0900

    feat: 근무일정 예외 클래스 추가 (#9)

---
+++ backend/src/main/java/com/mamdaero/domain/work_schedule/exception/WorkScheduleExceptionConstants.java
@@ -0,0 +1,19 @@
+package com.mamdaero.domain.work_schedule.exception;
+
+import com.mamdaero.global.exception.ExceptionConstants;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum WorkScheduleExceptionConstants implements ExceptionConstants {
+
+    WORK_SCHEDULE_NOT_FOUND("SCD001", HttpStatus.NOT_FOUND),
+    INVALID_TIME("SCD002", HttpStatus.BAD_REQUEST),
+    INVALID_DAY("SCD003", HttpStatus.BAD_REQUEST),
+    CAN_NOT_DELETE_WORK_SCHEDULE("SCD004", HttpStatus.BAD_REQUEST);
+
+    private final String code;
+    private final HttpStatus status;
+}
