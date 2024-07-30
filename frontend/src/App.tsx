8fb12479f0c96af01acaa752902055601b225822
허세령 <seryoii@naver.com>
2024-07-30 10:26:54 +0900

    design: 전체 배경색 적용 (#18)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import NavClient from '@/components/navigation/NavClient';
 import MainPage from '@/pages/MainPage';
 import SignUpChoose from '@/pages/SignUpChoose';
@@ -7,13 +7,31 @@ import SignUpCounselor from '@/pages/SignUpCounselor';
 import SignUpCounselorInfo from '@/components/input/SignUpCounselorInfo';
 import CounselorListPage from './pages/CounselorListPage';
 
+const routeBackgroundColors: { [key: string]: string } = {
+  '/signup/counselor': 'bg-blue-50',
+};
+
+function getBackgroundColor(pathname: string): string {
+  for (const [route, color] of Object.entries(routeBackgroundColors)) {
+    if (pathname.startsWith(route)) {
+      return color;
+    }
+  }
+  return 'bg-orange-50'; // 기본 배경색
+}
+
 function App() {
+  const location = useLocation();
+  const backgroundColor = getBackgroundColor(location.pathname);
+
   return (
     <div className="App grid grid-cols-8 min-h-screen">
       <div className="fixed flex min-h-screen col-span-1">
         <NavClient />
       </div>
-      <div className="col-start-2 col-span-7 justify-center items-center px-16 py-6">
+      <div
+        className={`col-start-2 col-span-7 justify-center items-center px-16 py-6 ${backgroundColor}`}
+      >
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/signup/choose" element={<SignUpChoose />} />
