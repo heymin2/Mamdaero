1ed315d0c7d53677ea9ac6182c39c270850851b6
박민준 <ppmm98@naver.com>
2024-07-31 16:35:04 +0900

    feat: diary Exception

---
+++ backend/src/main/java/com/mamdaero/domain/diary/exception/DiaryExceptionConstants.java
@@ -0,0 +1,19 @@
+package com.mamdaero.domain.diary.exception;
+
+import com.mamdaero.domain.global.exception.ExceptionConstants;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum DiaryExceptionConstants implements ExceptionConstants {
+
+    DIARY_NOT_FOUND("D001", HttpStatus.NOT_FOUND),
+    DIARY_BAD_REQUEST("D002", HttpStatus.BAD_REQUEST),
+    DIARY_NO_CONTENT("D003", HttpStatus.BAD_REQUEST),
+    DIARY_NO_DATE("D004", HttpStatus.BAD_REQUEST);
+
+    private final String code;
+    private final HttpStatus status;
+}
