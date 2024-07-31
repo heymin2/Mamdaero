486aff93d9476f2e3c37ad7a58e8cc4ae04f8e42
신혜민 <alddlv81@gmail.com>
2024-07-31 13:15:22 +0900

    feat: 익명 포스트 기능 (#22)

---
+++ backend/src/main/java/com/mamdaero/domain/postit/entity/Postit.java
@@ -0,0 +1,43 @@
+package com.mamdaero.domain.postit.entity;
+
+import com.mamdaero.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE post SET is_delete = true WHERE post_id = ?")
+@Where(clause = "is_delete = false")
+@Table(name = "post")
+public class Postit extends BaseEntity {
+
+    @Id
+    @Column(name = "post_id", updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @Column(name = "is_delete", nullable = false)
+    @ColumnDefault("false")
+    private Boolean isDelete;
+
+    @Builder
+    public Postit(Long memberId, String content) {
+        this.memberId = memberId;
+        this.content =content;
+        this.isDelete = false;
+    }
+}
