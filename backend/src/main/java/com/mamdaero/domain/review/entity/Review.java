02572ebc053ce71c4d118d41e635dea97d90ea3e
박형준 <issg2030@gmail.com>
2024-08-04 02:40:51 +0900

    comment: 상담보고서, 리뷰 주석처리 (#9)

--- backend/src/main/java/com/mamdaero/domain/review/entity/Review.java
+++ backend/src/main/java/com/mamdaero/domain/review/entity/Review.java
@@ -1,9 +1,10 @@
 package com.mamdaero.domain.review.entity;
 
-import com.mamdaero.domain.reservation.entity.Reservation;
 import com.mamdaero.domain.review.dto.request.ReviewRequestDto;
 import com.mamdaero.global.entity.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -18,10 +19,6 @@ public class Review extends BaseEntity {
     @Id
     @Column(name = "review_id")
     private Long id;
-    @OneToOne(fetch = FetchType.LAZY)
-    @MapsId
-    @JoinColumn(name = "review_id")
-    private Reservation reservation;
     @Column(nullable = false)
     private String review;
     @Column(nullable = false)
