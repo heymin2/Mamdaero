f26739222743ae6ed1e829a87e7381e610944c8b
신혜민 <alddlv81@gmail.com>
2024-08-04 23:46:27 +0900

    feat: 포스트잇 - 페이지네이션 적용 (#38)

--- backend/src/main/java/com/mamdaero/domain/postit/repository/PostitRepository.java
+++ backend/src/main/java/com/mamdaero/domain/postit/repository/PostitRepository.java
@@ -1,13 +1,14 @@
 package com.mamdaero.domain.postit.repository;
 
 import com.mamdaero.domain.postit.entity.Postit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface PoistitRepository extends JpaRepository<Postit, Long> {
-    List<Postit> findByQuestionId(Long questionId);
+public interface PostitRepository extends JpaRepository<Postit, Long> {
+    Page<Postit> findByQuestionId(Long questionId, Pageable pageable);
     Optional<Postit> findByQuestionIdAndIdAndMemberId(Long questionId, Long postitId, Long memberId);
     boolean existsByIdAndMemberId(Long id, Long memberId);
 }
