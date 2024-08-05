6e2820f3da3be3e6cfb7936285fa284eca8b0454
허세령 <seryoii@naver.com>
2024-08-05 15:12:36 +0900

    fix: 깃 오류 수정(2)

--- frontend/src/pages/signup/SignUpClient.tsx
+++ frontend/src/pages/signup/SignUpClient.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import SignUpClientInput from '@/components/input/SignUpClientInput';
+import SignUpClientCompleteCard from '@/components/card/signup/SignUpClientCompleteCard';
+import ProgressBar from '@/components/navigation/ClientProgressBar';
+import SignUpBar from '@/components/navigation/SignUpBar';
 
-const SignupClient: React.FC = () => {
+const SignUpClient: React.FC = () => {
+  const location = useLocation();
+  let currentStep: 'input' | 'complete' = 'input';
+
+  if (location.pathname.includes('complete')) {
+    currentStep = 'complete';
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen  w-full pt-28">
-      <h1 className="text-3xl font-bold mb-3">일반 회원 가입</h1>
-      <SignUpClientInput />
+    <div>
+      <div className="container space-y-10">
+        <SignUpBar
+          user="client"
+          title="일반 회원"
+          subtitle="맘대로와 함께 마음의 안정을 느껴보세요!"
+        />
+        <div className="flex flex-col w-full items-center">
+          <ProgressBar currentStep={currentStep} />
+          <Routes>
+            <Route path="/" element={<SignUpClientInput />} />
+            <Route path="complete" element={<SignUpClientCompleteCard />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default SignupClient;
+export default SignUpClient;
