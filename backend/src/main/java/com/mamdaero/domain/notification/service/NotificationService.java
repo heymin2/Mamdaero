958aeea5023091374fb164dc6f0ca881fcf77351
신혜민 <alddlv81@gmail.com>
2024-08-06 13:40:03 +0900

    feat: 예약 취소시 내담자, 상담사 둘다 알림 (#44)

--- backend/src/main/java/com/mamdaero/domain/notification/service/NotificationService.java
+++ backend/src/main/java/com/mamdaero/domain/notification/service/NotificationService.java
@@ -4,10 +4,14 @@ import com.mamdaero.domain.counselor_board.entity.CounselorBoard;
 import com.mamdaero.domain.counselor_board.entity.CounselorBoardComment;
 import com.mamdaero.domain.counselor_board.repository.CounselorBoardCommentRepository;
 import com.mamdaero.domain.counselor_board.repository.CounselorBoardRepository;
+import com.mamdaero.domain.counselor_item.entity.CounselorItem;
+import com.mamdaero.domain.counselor_item.repository.CounselorItemRepository;
 import com.mamdaero.domain.notification.controller.NotificationController;
 import com.mamdaero.domain.notification.entity.EventSource;
 import com.mamdaero.domain.notification.entity.Notification;
 import com.mamdaero.domain.notification.repository.NotificationRepository;
+import com.mamdaero.domain.reservation.entity.Reservation;
+import com.mamdaero.domain.reservation.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +29,8 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final CounselorBoardRepository counselorBoardRepository;
     private final CounselorBoardCommentRepository counselorBoardCommentRepository;
+    private final ReservationRepository reservationRepository;
+    private final CounselorItemRepository counselorItemRepository;
 
     // 메시지 알림
     public SseEmitter subscribe() {
@@ -97,6 +103,60 @@ public class NotificationService {
                 notification.setEventSource(EventSource.COUNSELOR_BOARD);
                 notification.setEventId(boardID);
                 notification.setMemberId(memberId);
+                notification.setIsDelete(false);
+                notification.setIsRead(false);
+
+                notificationRepository.save(notification);
+
+            } catch (IOException e) {
+                NotificationController.sseEmitters.remove(memberId);
+            }
+        }
+    }
+
+    // 상담사, 내담자 예약 취소
+    public void notifyCancelReservation(Long reservationId) {
+        Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(
+                () -> new IllegalArgumentException("예약을 찾을 수 없습니다.")
+        );
+
+        CounselorItem counselorItem = counselorItemRepository.findById(reservation.getCounselorItemId()).orElseThrow(
+                () -> new IllegalArgumentException("상담 상품을 찾을 수 없습니다.")
+        );
+
+        Long memberId = reservation.getMemberId();
+        Long counselorId = counselorItem.getCounselorId();
+
+        if (NotificationController.sseEmitters.containsKey(memberId)) {
+            SseEmitter sseEmitter = NotificationController.sseEmitters.get(memberId);
+            try {
+                Map<String, String> eventData = new HashMap<>();
+                eventData.put("message", "예약이 취소되었습니다.");
+                eventData.put("canceledAt", reservation.getCanceledAt().toString());
+                eventData.put("memberId", memberId.toString());
+                eventData.put("counselorId", counselorId.toString());
+
+                sseEmitter.send(SseEmitter.event().name("cancelReservation").data(eventData));
+
+                // 내담자
+                Notification notification = new Notification();
+                notification.setCreatedAt(reservation.getCanceledAt());
+                notification.setContent("예약이 취소되었습니다.");
+                notification.setEventSource(EventSource.RESERVATION);
+                notification.setEventId(reservationId);
+                notification.setMemberId(memberId);
+                notification.setIsDelete(false);
+                notification.setIsRead(false);
+
+                notificationRepository.save(notification);
+
+                //상담자
+                notification = new Notification();
+                notification.setCreatedAt(reservation.getCanceledAt());
+                notification.setContent("예약이 취소되었습니다.");
+                notification.setEventSource(EventSource.RESERVATION);
+                notification.setEventId(reservationId);
+                notification.setMemberId(counselorId);
                 notification.setIsDelete(false);
                 notification.setIsRead(false);
 
