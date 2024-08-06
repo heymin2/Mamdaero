c95e5eb5b9f5b3c50cb976bc6ea3ce2b751a5dea
박형준 <issg2030@gmail.com>
2024-08-06 13:13:13 +0900

    feat: 상담 음성데이터 저장(진행중) (#45)

--- backend/src/main/java/com/mamdaero/domain/MamdaeroApplication.java
+++ backend/src/main/java/com/mamdaero/domain/MamdaeroApplication.java
@@ -1,7 +1,9 @@
 package com.mamdaero.domain;
 
+import com.mamdaero.global.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 @ComponentScan(basePackages = {"com.mamdaero.global", "com.mamdaero"})
+@EnableConfigurationProperties(FileStorageProperties.class)
 @SpringBootApplication
 public class MamdaeroApplication {
 
