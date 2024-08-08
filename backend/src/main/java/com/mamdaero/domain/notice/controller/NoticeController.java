179a9b44280ed8e9969ab3cd7eae2c8f85f7c6a5
박민준 <ppmm98@naver.com>
2024-08-08 17:50:20 +0900

    refactor: Notice

--- backend/src/main/java/com/mamdaero/domain/notice/controller/NoticeController.java
+++ backend/src/main/java/com/mamdaero/domain/notice/controller/NoticeController.java
@@ -1,11 +1,16 @@
 package com.mamdaero.domain.notice.controller;
 
+import com.mamdaero.domain.member.security.service.FindUserService;
 import com.mamdaero.domain.notice.dto.request.NoticeRequest;
 import com.mamdaero.domain.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,21 +18,33 @@ import org.springframework.web.bind.annotation.*;
 public class NoticeController {
 
     private final NoticeService noticeService;
+    private final FindUserService findUserService;
 
     @PostMapping
     public ResponseEntity<?> create(@RequestBody NoticeRequest request) {
-        noticeService.create(request);
-        return ResponseEntity.ok().build();
+        if (Objects.equals(findUserService.findMemberRole(), "관리자")) {
+            noticeService.create(findUserService.findMemberId(), request);
+            return ResponseEntity.ok().build();
+        }
+
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
     }
 
     @PatchMapping("/{noticeId}")
     public ResponseEntity<?> update(@PathVariable("noticeId") Long id, @RequestBody NoticeRequest request) {
-        return ResponseEntity.ok(noticeService.update(id, request));
+
+        if (Objects.equals(findUserService.findMemberRole(), "관리자")) {
+            return ResponseEntity.ok(noticeService.update(findUserService.findMemberId(), id, request));
+        }
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
     }
 
     @DeleteMapping("/{noticeId}")
     public ResponseEntity<?> delete(@PathVariable("noticeId") Long id) {
-        noticeService.delete(id);
+        if (Objects.equals(findUserService.findMemberRole(), "관리자")) {
+            noticeService.delete(findUserService.findMemberId(), id);
         return ResponseEntity.ok().build();
+        }
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
     }
 }
