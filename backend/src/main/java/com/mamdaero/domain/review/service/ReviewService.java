1ecc15bf42401b8483240eefe3fa9f426b47a759
brobac <issg2030@gmail.com>
2024-08-08 22:45:54 +0900

    feat: 내 리뷰 조회 권한, 멤버 로직 추가 (#61)

--- backend/src/main/java/com/mamdaero/domain/review/service/ReviewService.java
+++ backend/src/main/java/com/mamdaero/domain/review/service/ReviewService.java
@@ -1,6 +1,9 @@
 package com.mamdaero.domain.review.service;
 
 import com.mamdaero.domain.consult.repository.ConsultRepository;
+import com.mamdaero.domain.member.exception.AccessDeniedException;
+import com.mamdaero.domain.member.security.dto.MemberInfoDTO;
+import com.mamdaero.domain.member.security.service.FindUserService;
 import com.mamdaero.domain.reservation.repository.ReservationRepository;
 import com.mamdaero.domain.review.dto.request.CreateReviewRequest;
 import com.mamdaero.domain.review.dto.request.UpdateReviewRequest;
@@ -25,6 +28,8 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final ConsultRepository consultRepository;
     private final ReservationRepository reservationRepository;
+    private final FindUserService findUserService;
+
 
     public Pagination<ReviewResponse> findAllCounselorReview(Long id, int page, int size) {
 
@@ -43,12 +48,15 @@ public class ReviewService {
     }
 
     public Pagination<ReviewResponse> findAllMyReview(int page, int size) {
-        //TODO: 진짜 멤버 아이디로 바꾸기
-        Long memberId = 1L;
+
+        MemberInfoDTO member = findUserService.findMember();
+        if(member == null) {
+            throw new AccessDeniedException();
+        }
 
         Pageable pageable = PageRequest.of(page, size);
 
-        Page<ReviewResponse> reviewPage = reviewRepository.findAllMyReview(memberId, pageable);
+        Page<ReviewResponse> reviewPage = reviewRepository.findAllMyReview(member.getMemberId(), pageable);
 
         return new Pagination<>(
                 reviewPage.getContent(),
