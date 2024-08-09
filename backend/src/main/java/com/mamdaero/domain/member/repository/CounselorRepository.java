e8bc8c643c792fbec7a02c4fade1e6e2706ca288
박민준 <ppmm98@naver.com>
2024-08-09 10:44:06 +0900

    refactor: Counselor With Paging

--- backend/src/main/java/com/mamdaero/domain/member/repository/CounselorRepository.java
+++ backend/src/main/java/com/mamdaero/domain/member/repository/CounselorRepository.java
@@ -1,11 +1,15 @@
 package com.mamdaero.domain.member.repository;
 
 import com.mamdaero.domain.member.entity.Counselor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface CounselorRepository extends JpaRepository<Counselor, Long> {
 
-    List<Counselor> findAllByNameContains(String counselorName);
+//    List<Counselor> findAllByNameContains(String counselorName);
+
+//    Page<Counselor> findAll(Pageable pageable);
+
+    Page<Counselor> findAllByNameContains(String counselorName, Pageable pageable);
 }
